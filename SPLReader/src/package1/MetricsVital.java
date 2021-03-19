7e3482d3e09bbdedd829d1738ce87276d9d62a9b
Kaci Ahmed <kaciahmed3@gmail.com>
2021-03-19 12:15:50 +0100

    immplementation de la métric vital Var Fan-out on VPG

--- SPLReader/src/package1/MetricsVital.java
+++ SPLReader/src/package1/MetricsVital.java
@@ -1,34 +1,40 @@
 package package1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement(name = "metricsVital")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(propOrder = { "moyenneDegree", "DispersionPredicatsDansLesAnnotations", "RapporPredicatsParGroupesAnnotations",
-		"RapporPredicatsfichierParNbTotalFichier", "moyenneNbPredicatTousLesFichierParNbTotalFichier",
-		"RapportAnnotationDifferenteParAnnotationTotal" })
 public class MetricsVital {
 
 	private float moyenneDegree; // sum(moyenne degree) / nbAnnotationsGroupe
-	private float DispersionPredicatsDansLesAnnotations; // nbPredicatTotal / nbAnnotationTotal
-	private float RapporPredicatsParGroupesAnnotations; // nbPredicatTotalDeTousLesVPGs / nbVpgTotal // var
-														// fan-out-onVPG
-	private float RapporPredicatsfichierParNbTotalFichier; // (nbPredicatsParFichier / NbFichier) d'un fichier // var
-															// fan-in-on file
-	private float moyenneNbPredicatTousLesFichierParNbTotalFichier; // nbPredicatsTotalDeTousLesFichier / nbFichiers //
-																	// Var fan-out-on file
-	private float RapportAnnotationDifferenteParAnnotationTotal; // nbAnnotationsDifferent / nbAnnotationsTotal d'un
-																	// fichier // VP Fan in on file
+	private float dispersionPredicatsDansLesAnnotations; // nbPredicatTotal / nbAnnotationTotal
+
+	private float avgVarFanOutOnVPG; // nbPredicatTotalDeTousLesVPGs / nbVpgTotal // var
+										// fan-out-onVPG
+	private float avgVarFanOutOnFile; // (nbPredicatsParFichier / NbFichier) d'un fichier // var
+										// fan-in-on file
+	private float avgVarFanInOnFile; // nbPredicatsTotalDeTousLesFichier / nbFichiers //
+										// Var fan-out-on file
+	private float avgVpFanInOnFile; // nbAnnotationsDifferent / nbAnnotationsTotal d'un
+	@XmlTransient // fichier // VP Fan in on file
+	private Set<Predicat> ensemblePredicats;
+	@XmlTransient
+	private Map<Predicat, List<AnnotationGroupe>> appartenancePredicatVPG;
 
 	public MetricsVital() {
 		super();
+		this.ensemblePredicats = new HashSet<>();
+		this.appartenancePredicatVPG = new HashMap<Predicat, List<AnnotationGroupe>>();
 	}
 
 	public MetricsVital(float moyenneDegree, float dispersionPredicatsDansLesAnnotations,
@@ -37,60 +43,13 @@ public class MetricsVital {
 			float rapportAnnotationDifferenteParAnnotationTotal) {
 		super();
 		this.moyenneDegree = moyenneDegree;
-		this.DispersionPredicatsDansLesAnnotations = dispersionPredicatsDansLesAnnotations;
-		this.RapporPredicatsParGroupesAnnotations = rapporPredicatsParGroupesAnnotations;
-		this.RapporPredicatsfichierParNbTotalFichier = rapporPredicatsfichierParNbTotalFichier;
-		this.moyenneNbPredicatTousLesFichierParNbTotalFichier = moyenneNbPredicatTousLesFichierParNbTotalFichier;
-		this.RapportAnnotationDifferenteParAnnotationTotal = rapportAnnotationDifferenteParAnnotationTotal;
-	}
-
-	public float getMoyenneDegree() {
-		return this.moyenneDegree;
-	}
-
-	public void setMoyenneDegree(float moyenneDegree) {
-		this.moyenneDegree = moyenneDegree;
-	}
-
-	public float getDispersionPredicatsDansLesAnnotations() {
-		return this.DispersionPredicatsDansLesAnnotations;
-	}
-
-	public void setDispersionPredicatsDansLesAnnotations(float dispersionPredicatsDansLesAnnotations) {
-		this.DispersionPredicatsDansLesAnnotations = dispersionPredicatsDansLesAnnotations;
-	}
-
-	public float getRapporPredicatsParGroupesAnnotations() {
-		return this.RapporPredicatsParGroupesAnnotations;
-	}
-
-	public void setRapporPredicatsParGroupesAnnotations(float rapporPredicatsParGroupesAnnotations) {
-		this.RapporPredicatsParGroupesAnnotations = rapporPredicatsParGroupesAnnotations;
-	}
-
-	public float getRapporPredicatsfichierParNbTotalFichier() {
-		return this.RapporPredicatsfichierParNbTotalFichier;
-	}
-
-	public void setRapporPredicatsfichierParNbTotalFichier(float rapporPredicatsfichierParNbTotalFichier) {
-		this.RapporPredicatsfichierParNbTotalFichier = rapporPredicatsfichierParNbTotalFichier;
-	}
-
-	public float getMoyenneNbPredicatTousLesFichierParNbTotalFichier() {
-		return this.moyenneNbPredicatTousLesFichierParNbTotalFichier;
-	}
-
-	public void setMoyenneNbPredicatTousLesFichierParNbTotalFichier(
-			float moyenneNbPredicatTousLesFichierParNbTotalFichier) {
-		this.moyenneNbPredicatTousLesFichierParNbTotalFichier = moyenneNbPredicatTousLesFichierParNbTotalFichier;
-	}
-
-	public float getRapportAnnotationDifferenteParAnnotationTotal() {
-		return this.RapportAnnotationDifferenteParAnnotationTotal;
-	}
-
-	public void setRapportAnnotationDifferenteParAnnotationTotal(float rapportAnnotationDifferenteParAnnotationTotal) {
-		this.RapportAnnotationDifferenteParAnnotationTotal = rapportAnnotationDifferenteParAnnotationTotal;
+		this.dispersionPredicatsDansLesAnnotations = dispersionPredicatsDansLesAnnotations;
+		this.avgVarFanOutOnVPG = rapporPredicatsParGroupesAnnotations;
+		this.avgVarFanOutOnFile = rapporPredicatsfichierParNbTotalFichier;
+		this.avgVarFanInOnFile = moyenneNbPredicatTousLesFichierParNbTotalFichier;
+		this.avgVpFanInOnFile = rapportAnnotationDifferenteParAnnotationTotal;
+		this.ensemblePredicats = new HashSet<>();
+		this.appartenancePredicatVPG = new HashMap<Predicat, List<AnnotationGroupe>>();
 	}
 
 	public void calculerMetrics(List<AnnotationSynthese> annotationsSyntheses, Set<AnnotationGroupe> annotationsGroupes,
@@ -98,8 +57,11 @@ public class MetricsVital {
 		// TODO
 		calculerMoyenneDegree(annotationsSyntheses);
 		calculerDispersionPredicatDansLesAnnotations(annotationsLineariser);
-		calculerRapporPredicatsParGroupesAnnotations(annotationsGroupes);
-		calculerRapporPredicatsfichierParNbTotalFichier(annotationsLineariser, nbFichierParser);
+		recupererTousPredicatsProjet(annotationsGroupes);
+		calculerAvgVarFanOutOnVPG(annotationsGroupes);
+		// calculerRapporPredicatsParGroupesAnnotations(annotationsGroupes);
+		// calculerRapporPredicatsfichierParNbTotalFichier(annotationsLineariser,
+		// nbFichierParser);
 		// moyenneNbPredicatTousLesFichierParNbTotalFichier; //
 		// nbPredicatsTotalDeTousLesFichier / nbFichiers // Var fan-out-on file
 		// RapportAnnotationDifferenteParAnnotationTotal; // nbAnnotationsDifferent /
@@ -129,52 +91,128 @@ public class MetricsVital {
 		setDispersionPredicatsDansLesAnnotations(resultat);
 	}
 
-	public void calculerRapporPredicatsParGroupesAnnotations(Set<AnnotationGroupe> annotationsGroupes) {
-		float resultat = 0;
-		int cptPredicatTotal = 0;
-		final int nbAnnotationGroupeTotal = annotationsGroupes.size();
-		Proposition propositionTemporaire;
-		List<Predicat> predicatsTemporaire;
+	/*
+	 * récupurer tous les prédicats du projet //set cacluer le nombre total de
+	 * groupes annotations pour chaque prédicats chercher la liste des Groupes
+	 * d'annotations qui le contient // caluler la cardinalité des de la liste des
+	 * Groupes d'annotations diviser la cardinalité par le nombre de groupe
+	 * d'annotation et stocké le résultat dans une liste sommet les élément de la
+	 * liste et le diviser par le nombre d'élément de la liste
+	 */
+	public float calculerMoyenneParPredicat(Predicat predicat, int nbTotalGroupeAnnotation) {
+		List<AnnotationGroupe> lstAnnotationGroupeOfPredicat = this.appartenancePredicatVPG.get(predicat);
+		int nbOccurance = 0;
+		for (AnnotationGroupe annotationGroupe : lstAnnotationGroupeOfPredicat) {
+			for (Predicat predicat2 : annotationGroupe.getProposition().getPredicats()) {
+				if (predicat.equals(predicat2)) {
+					nbOccurance++;
+				}
+			}
+		}
+		return nbOccurance / nbTotalGroupeAnnotation;
+	}
+
+	public boolean verifierAppartenancePredicat(Predicat predicat, AnnotationGroupe annotationGroupe) {
+		return annotationGroupe.getProposition().getPredicats().contains(predicat);
+	}
+
+	public void remplirAppratenancePredicatVPG(Set<AnnotationGroupe> annotationsGroupes) {
+		List<AnnotationGroupe> annotationGroupesOfPredicat;
+		for (Predicat predicat : this.ensemblePredicats) {
+			annotationGroupesOfPredicat = new ArrayList<>();
+			for (AnnotationGroupe annotationGroupe : annotationsGroupes) {
+				if (verifierAppartenancePredicat(predicat, annotationGroupe)) {
+					annotationGroupesOfPredicat.add(annotationGroupe);
+				}
+			}
+			this.appartenancePredicatVPG.put(predicat, annotationGroupesOfPredicat);
+		}
+	}
+
+	public void recupererTousPredicatsProjet(Set<AnnotationGroupe> annotationsGroupes) {
 		for (AnnotationGroupe annotationGroupe : annotationsGroupes) {
-			propositionTemporaire = annotationGroupe.getProposition();
-			predicatsTemporaire = propositionTemporaire.getPredicats();
-			cptPredicatTotal += predicatsTemporaire.size();
-		}
-		resultat = cptPredicatTotal / nbAnnotationGroupeTotal;
-		setRapporPredicatsParGroupesAnnotations(resultat);
-	}
-
-	public void calculerRapporPredicatsfichierParNbTotalFichier(List<Annotation> annotationsLineariser,
-			int nombreFichierTotal) {
-		float resultat = 0;
-		int nbPredicatFichierCourant = annotationsLineariser.get(0).getProposition().getPredicats().size();
-		;
-		List<Integer> nbPredicatsParFichier = new ArrayList<>();
-
-		int indiceDernier = annotationsLineariser.size() - 1;
-		boolean estMemeFichier;
-		boolean estAvantDernier;
-
-		for (int i = 1; i < indiceDernier; i++) {
-			String nomFichierSuivant = annotationsLineariser.get(i + 1).getNomDeFichier();
-			estMemeFichier = annotationsLineariser.get(i).getNomDeFichier().equals(nomFichierSuivant);
-			if (estMemeFichier) {
-				nbPredicatFichierCourant += annotationsLineariser.get(i).getProposition().getPredicats().size();
-			} else {
-				nbPredicatFichierCourant += annotationsLineariser.get(i).getProposition().getPredicats().size();
-				nbPredicatsParFichier.add(nbPredicatFichierCourant);
-				nbPredicatFichierCourant = 0;
-			}
-
-			estAvantDernier = ((i + 1) == indiceDernier);
-			if (estAvantDernier) {
-				nbPredicatFichierCourant += annotationsLineariser.get(indiceDernier).getProposition().getPredicats()
-						.size();
-				nbPredicatsParFichier.add(nbPredicatFichierCourant);
+			for (Predicat predicat : annotationGroupe.getProposition().getPredicats()) {
+				this.ensemblePredicats.add(predicat);
 			}
 		}
-
-		resultat = nbPredicatsParFichier.stream().mapToInt(e -> e).sum() / nombreFichierTotal;
-		setRapporPredicatsfichierParNbTotalFichier(resultat);
 	}
+
+	public void calculerAvgVarFanOutOnVPG(Set<AnnotationGroupe> annotationsGroupes) {
+		recupererTousPredicatsProjet(annotationsGroupes);
+		int nbTotalGroupeAnnotation = annotationsGroupes.size();
+		remplirAppratenancePredicatVPG(annotationsGroupes);
+		List<Float> listMoyenneParPredicat = new ArrayList<>();
+		for (Predicat predicat : this.ensemblePredicats) {
+			listMoyenneParPredicat.add(Float.valueOf(calculerMoyenneParPredicat(predicat, nbTotalGroupeAnnotation)));
+		}
+		Double resultat = listMoyenneParPredicat.stream().mapToDouble(e -> e).sum() / listMoyenneParPredicat.size();
+		this.setAvgVarFanOutOnVPG(resultat.floatValue());
+
+	}
+
+	// Getter && Setter
+	public float getMoyenneDegree() {
+		return this.moyenneDegree;
+	}
+
+	public void setMoyenneDegree(float moyenneDegree) {
+		this.moyenneDegree = moyenneDegree;
+	}
+
+	public float getDispersionPredicatsDansLesAnnotations() {
+		return this.dispersionPredicatsDansLesAnnotations;
+	}
+
+	public void setDispersionPredicatsDansLesAnnotations(float dispersionPredicatsDansLesAnnotations) {
+		this.dispersionPredicatsDansLesAnnotations = dispersionPredicatsDansLesAnnotations;
+	}
+
+	public Set<Predicat> getEnsemblePredicats() {
+		return this.ensemblePredicats;
+	}
+
+	public void setEnsemblePredicats(Set<Predicat> ensemblePredicats) {
+		this.ensemblePredicats = ensemblePredicats;
+	}
+
+	public Map<Predicat, List<AnnotationGroupe>> getAppartenancePredicatVPG() {
+		return this.appartenancePredicatVPG;
+	}
+
+	public void setAppartenancePredicatVPG(Map<Predicat, List<AnnotationGroupe>> appartenancePredicatVPG) {
+		this.appartenancePredicatVPG = appartenancePredicatVPG;
+	}
+
+	public float getAvgVarFanOutOnVPG() {
+		return this.avgVarFanOutOnVPG;
+	}
+
+	public void setAvgVarFanOutOnVPG(float avgVarFanOutOnVPG) {
+		this.avgVarFanOutOnVPG = avgVarFanOutOnVPG;
+	}
+
+	public float getAvgVarFanOutOnFile() {
+		return this.avgVarFanOutOnFile;
+	}
+
+	public void setAvgVarFanOutOnFile(float avgVarFanOutOnFile) {
+		this.avgVarFanOutOnFile = avgVarFanOutOnFile;
+	}
+
+	public float getAvgVarFanInOnFile() {
+		return this.avgVarFanInOnFile;
+	}
+
+	public void setAvgVarFanInOnFile(float avgVarFanInOnFile) {
+		this.avgVarFanInOnFile = avgVarFanInOnFile;
+	}
+
+	public float getAvgVpFanInOnFile() {
+		return this.avgVpFanInOnFile;
+	}
+
+	public void setAvgVpFanInOnFile(float avgVpFanInOnFile) {
+		this.avgVpFanInOnFile = avgVpFanInOnFile;
+	}
+
 }
